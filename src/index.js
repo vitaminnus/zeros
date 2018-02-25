d97d5e1911282ff912613b79bd49afdb32d0875f
Viktor Dmitriev <vitaminnus@gmail.com>
2018-02-25 10:48:16 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  let count = 0;   
+  let fives;
+  for (let five=5; five<number;five=five*5) {
+    fives = Math.floor(number/five);
+    count = count + fives;    
+  }
+  return count;
 }
